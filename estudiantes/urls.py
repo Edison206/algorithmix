f159b15033d9ba1e8843d0d0fda6f58a37fba283
Edison206 <edypaulandeta@gmail.com>
2026-06-09 23:50:47 -0500

    Fix agregar ruta bienvenida y unificar nombre a ALGORITHMIX

--- estudiantes/urls.py
+++ estudiantes/urls.py
@@ -6,12 +6,11 @@ urlpatterns = [
     path('login/verify/', login_verify),
     path('registro/', registro, name='registro'),
     path('bienvenida/', bienvenida, name='bienvenida'),
-    path('temas/', temas,  name='temas'),
+    path('temas/', temas, name='temas'),
     path('ejercicios/<int:id>/', ejercicios, name='ejercicios'),
     path('ejercicio_resolver/<int:id>/', ejercicioResolver, name='ejercicio_resolver'),
     path('agregar_respuesta/', agregar_respuesta),
     path('ejercicio_aprender/<int:id>/', ejercicioAprender, name='ejercicio_aprender'),
     path('logout/', cerrar_sesion, name='logout'),
     path('reporte/descargar/', descargar_reporte, name='descargar_reporte'),
-
 ]
